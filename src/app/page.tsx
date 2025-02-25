39f99f397ab4ad8b1f332774cb1d5c8c2af6fbcb
PedroMakengo <pedromakengomiguel@gmail.com>
2025-02-25 08:18:00 +0000

    Adicionado a parte do about

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import { About } from './_components/About'
 import { Hero } from './_components/hero'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   )
 }
